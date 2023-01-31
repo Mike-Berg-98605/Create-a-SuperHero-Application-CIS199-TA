4817292fa4c661d8a2ef7e34fe3930fb89c66103
Mike Berg <68578047+Mike-Berg-UL@users.noreply.github.com>
2023-01-31 13:34:01 -0500

    main

---
+++ Prog4/Program.cs
@@ -0,0 +1,72 @@
+﻿// Program 4
+// CIS 199-50
+// Due: 12/1/2021
+
+
+// File: Program.cs
+// This file creates a simple test application class that creates
+// an array of Superhero objects and tests them.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+
+public class Program
+{
+    // Precondition:  None
+    // Postcondition: The Superhero class has been tested
+    public static void Main(string[] args)
+    {
+        Superhero superHero1 = new Superhero("Aquaman", "Atlantis", "Telepathic control of aquatic life",
+            1941, "Utilizing The Sacred Trident");  // 1st test superhero
+        Superhero superHero2 = new Superhero("Batman", "Gotham City", "Genius-level intellect",
+            1939, "Master of Martial Arts"); // 2nd test superhero
+        Superhero superHero3 = new Superhero("Iron Man", "Long Island", "Powered Armor Suit",
+            1963, "Supersonic Flight"); // 3rd test superhero
+        Superhero superHero4 = new Superhero("Green Lantern", "Coast City", "Flight",
+            1940, "Semi Invulnerability");    // 4th test superhero
+        Superhero superHero5 = new Superhero("Spider-Man", "Queens", "Superhuman Strength",
+            1962, "Spidey Senses"); // 5th test superhero
+
+        Superhero[] superHeros = { superHero1, superHero2, superHero3, superHero4, superHero5 }; // Test array of superheroes
+
+        WriteLine("Original list of super heros");
+        WriteLine("----------------------");
+        PrintBooks(superHeros);
+
+        // Make changes
+        superHero1.OnPlanetEarth();
+        superHero2.FirstSuperpower = "Super Strength";
+        superHero3.OnPlanetEarth();
+        superHero4.SecondSuperpower = "Hard-Light Generation";
+        superHero5.OnPlanetEarth();
+        superHero5.BirthYear = -1234; // Attempt invalid year
+
+        WriteLine("After changes");
+        WriteLine("-------------");
+        PrintBooks(superHeros);
+
+        // superhero is off planet earth
+        superHero1.OffPlanetEarth();
+        superHero3.OffPlanetEarth();
+        superHero5.OffPlanetEarth();
+
+        WriteLine("After super hero leaves planet Earth");
+        WriteLine("-------------------------");
+        PrintBooks(superHeros);
+    }
+
+    // Precondition:  None
+    // Postcondition: The superheroes have been printed to the console
+    public static void PrintBooks(Superhero[] superHero)
+    {
+        foreach (Superhero sh in superHero)
+        {
+            WriteLine(sh);
+            WriteLine();
+        }
+    }
+}
---
+++ Prog4/Superhero.cs
@@ -0,0 +1,171 @@
+﻿
+// This file creates a simple Superhero class capable of tracking
+// the Superhero's name, city of birth, a first superpower, year of birth, a second superpower,
+// and on planet earth status. Classes, constructors, getters, setters, properties, backing fields, functions that return a bool,
+// and an override of the ToString method are used.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Superhero
+{
+    public const int BIRTH_YEAR = 1999; // Default birth year
+
+    private string _name;      // The superhero's name
+    private string _birthCity;     // The superhero's city of birth
+    private string _firstSuperPower;  // The superhero's first super power
+    private int _birthYear; // The superhero's year of birth
+    private string secondSuperPower; // The superhero's second super power
+    private bool _onPlanetEarth;   // The superhero's on planet Earth status
+
+    // Precondition:  the birth year >= 0
+    // Postcondition: The superhero has been initialized with the specified
+    //                values for name, birth city, first superpower, birth year, and
+    //                second superpower. The superhero is not on planet Earth.
+    public Superhero(String superheroName, String birthCity, String firstSuperPower,
+        int birthYear, String secondSuperPower)
+    {
+        SuperheroName = superheroName;
+        BirthCity = birthCity;
+        FirstSuperpower = firstSuperPower;
+        BirthYear = birthYear;
+        SecondSuperpower = secondSuperPower;
+
+        OffPlanetEarth(); // Make sure superhero is not on planet Earth
+    }
+
+    public string SuperheroName
+    {
+        // Precondition:  None
+        // Postcondition: The name has been returned
+        get
+        {
+            return _name;
+        }
+
+        // Precondition:  None
+        // Postcondition: The name has been set to the specified value
+        set
+        {
+            _name = value;
+        }
+    }
+
+    public char Initial
+    {
+        get
+        {
+            return SuperheroName[0];
+        }
+    }
+
+    public string BirthCity
+    {
+        // Precondition:  None
+        // Postcondition: The city has been returned
+        get
+        {
+            return _birthCity;
+        }
+
+        // Precondition:  None
+        // Postcondition: The city has been set to the specified value
+        set
+        {
+            _birthCity = value;
+        }
+    }
+
+    public string FirstSuperpower
+    {
+        // Precondition:  None
+        // Postcondition: The first superpower has been returned
+        get
+        {
+            return _firstSuperPower;
+        }
+
+        // Precondition:  None
+        // Postcondition: The superhero has been set to the specified value
+        set
+        {
+            _firstSuperPower = value;
+        }
+    }
+
+    public int BirthYear
+    {
+        // Precondition:  None
+        // Postcondition: The birth year has been returned
+        get
+        {
+            return _birthYear;
+        }
+
+        // Precondition:  value >= 0
+        // Postcondition: The birth year has been set to the specified value
+        set
+        {
+            if (value >= 0)
+                _birthYear = value;
+            else
+                _birthYear = BIRTH_YEAR;
+        }
+    }
+
+    public string SecondSuperpower
+    {
+        // Precondition:  None
+        // Postcondition: The second superpower has been returned
+        get
+        {
+            return secondSuperPower;
+        }
+
+        // Precondition:  None
+        // Postcondition: The second superpower has been set to the specified value
+        set
+        {
+            secondSuperPower = value;
+        }
+    }
+
+    // Precondition:  None
+    // Postcondition: The superhero is on planet Earth
+    public void OnPlanetEarth()
+    {
+        _onPlanetEarth = true;
+    }
+
+    // Precondition:  None
+    // Postcondition: The superhero is off planet Earth
+    public void OffPlanetEarth()
+    {
+        _onPlanetEarth = false;
+    }
+
+    // Precondition:  None
+    // Postcondition: true is returned if the superhero is on planet earth,
+    //                otherwise false is returned
+    public bool IsOnPlanetEarth()
+    {
+        return _onPlanetEarth;
+    }
+
+    // Precondition:  None
+    // Postcondition: A string is returned representing the superhero's
+    //                data on separate lines
+    public override string ToString()
+    {
+        string NL = Environment.NewLine; // Newline shortcut
+
+        return $"Name: {SuperheroName}{NL}City: {BirthCity}{NL}First Super Power: {FirstSuperpower}{NL}" +
+            $"Born in: {BirthYear}{NL}Second Super Power: {SecondSuperpower}{NL}" +
+            $"Planet Earth: {IsOnPlanetEarth()}{NL}" +
+            $"Initial: {Initial}";
+    }
+}
